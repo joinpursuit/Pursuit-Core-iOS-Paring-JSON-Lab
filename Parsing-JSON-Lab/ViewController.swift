63c6bb4624f30722a5b97bf31ca7b7a966165b4a
Eric Widjaja <ericwidjaja@pursuit.org>
2019-08-27 15:06:51 -0400

    Initial Commit

---
+++ Parsing-JSON-Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parsing-JSON-Lab
+//
+//  Created by Eric Widjaja on 8/27/19.
+//  Copyright © 2019 Eric Widjaja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
