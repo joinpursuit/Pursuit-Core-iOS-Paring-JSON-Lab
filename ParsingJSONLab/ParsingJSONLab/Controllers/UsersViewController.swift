b369deb92a7be953533fa10a549d1a6a083093d8
Sam <samroman3@gmail.com>
2019-08-27 14:42:12 -0400

    Created json files in parent directory. Added controllers for User, Color, and Weather views

---
+++ ParsingJSONLab/ParsingJSONLab/Controllers/UsersViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UsersViewController.swift
+//  ParsingJSONLab
+//
+//  Created by Sam Roman on 8/27/19.
+//  Copyright © 2019 Sam Roman. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
