554f70445f822d4f92a1c3e0bd184dfc013e3525
Yuliia Engman <yuliia.engman@gmail.com>
2019-11-26 14:53:30 -0500

    set VC

---
+++ ParsingJSON/ParsingJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParsingJSON
+//
+//  Created by Yuliia Engman on 11/26/19.
+//  Copyright © 2019 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ParsingJSON/ParsingJSON/WeatherDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherDetailViewController.swift
+//  ParsingJSON
+//
+//  Created by Yuliia Engman on 11/26/19.
+//  Copyright © 2019 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+class WeatherDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
