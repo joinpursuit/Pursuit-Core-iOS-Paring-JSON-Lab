62e7949aa2828f001f6e4f4700a29cc397ab8d4b
Yuliia Engman <yuliia.engman@gmail.com>
2019-11-27 15:16:18 -0500

    added files for colors

---
+++ ParsingJSON/ParsingJSON/ColorViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ColorViewController.swift
+//  ParsingJSON
+//
+//  Created by Yuliia Engman on 11/26/19.
+//  Copyright © 2019 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+class ColorViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
